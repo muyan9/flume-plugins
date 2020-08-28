4a2f4091c7895c44dff4e3e02009a60c28ca270b
zhaochangyu@antiy.cn <zhaochangyu@antiy.cn>
2020-08-28 16:27:49 +0800

    add readme

--- flume-HttpSource-PostFileHandler/src/zcy/flume/httpsource/handler/postfile/HttpSourcePostFileHanlder.java
+++ flume-HttpSource-PostFileHandler/src/zcy/flume/httpsource/handler/postfile/HttpSourcePostFileHanlder.java
@@ -1,6 +1,8 @@
 package zcy.flume.httpsource.handler.postfile;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Enumeration;
@@ -11,6 +13,7 @@ import javax.servlet.http.Part;
 import org.apache.flume.Context;
 import org.apache.flume.Event;
 import org.apache.flume.event.EventBuilder;
+import org.apache.flume.source.SpoolDirectorySource;
 import org.apache.flume.source.http.HTTPBadRequestException;
 import org.apache.flume.source.http.HTTPSourceHandler;
 import org.eclipse.jetty.server.Request;
@@ -24,13 +27,30 @@ public class HttpSourcePostFileHanlder implements HTTPSourceHandler {
 	public void configure(Context context) {
 		
 	}
+	
+	public static String md5(byte[] data) {
+		try {
+			MessageDigest m = MessageDigest.getInstance("MD5");
+			m.update(data);
+			byte s[] = m.digest();
+			String result = "";
+			for (int i = 0; i < s.length; i++) {
+				result += Integer.toHexString((0x000000FF & s[i]) | 0xFFFFFF00).substring(6);
+			}
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "";
+	}
 
 	@Override
 	public List<Event> getEvents(HttpServletRequest request) throws HTTPBadRequestException, Exception {
 		request.setAttribute(Request.__MULTIPART_CONFIG_ELEMENT, MULTI_PART_CONFIG);
 		List<Event> events = null;
 		String contentType = request.getContentType();
-		LOG.info(contentType);
+		LOG.debug(contentType);
 		
 		if(contentType != null && contentType.startsWith("multipart/")) {
 			Collection<Part> parts = request.getParts();
@@ -39,9 +59,18 @@ public class HttpSourcePostFileHanlder implements HTTPSourceHandler {
 			    int l = (int) part.getSize();
 			    InputStream input = part.getInputStream();
 			    
-			    byte[] baFileContent = new byte[l];
-			    input.read(baFileContent);
-			    events.add(EventBuilder.withBody(baFileContent));
+				ByteArrayOutputStream out = new ByteArrayOutputStream(l);
+                byte[] doc = new byte[512];
+                int n;
+                //若没有读到，即读取到末尾，则返回-1
+                while((n=input.read(doc,0,512))!=-1)
+                {
+                    //这就把读取到的n个字节全部都写入到指定路径了
+                    out.write(doc,0,n);
+                }
+                byte[] bb = out.toByteArray();
+                LOG.warn(md5(bb));
+			    events.add(EventBuilder.withBody(bb));
 			}
 		}else {
 			Enumeration<String> e = request.getParameterNames();
